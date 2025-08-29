95b0998523086678672618ba6d120f16330269e2
Italo dos Santos Ferreira <iferreira@iff.edu.br>
2025-08-29 13:58:15 -0300

    Organizing help and code.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# vm-plone-portal-padrao-2.1.1
-Virtual machine for Plone Portal Padrão 2.1.1
+# About
+This is a script to create a virtual machine for Plone Portal Padrão version 2.1.1.
+
+# References
+https://github.com/plone/plone.docker/blob/master/4.3/4.3.19/debian/Dockerfile
+https://github.com/plonegovbr/portalpadrao.docker/blob/v2.1.1/Dockerfile
--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 PIP=9.0.3
 ZC_BUILDOUT=2.13.1
-SETUPTOOLS=40.8.0
+#SETUPTOOLS=40.8.0 # Versão do Plone Puro
+SETUPTOOLS=38.7.0 # Versão do Portal Padrão
 WHEEL=0.33.1
 PLONE_MAJOR=4.3
 PLONE_VERSION=4.3.19
 PLONE_MD5=04ed5beac7fb8504f06a36d44e407b06
+PORTAL_PADRAO=2.1.1
+buildDeps="dpkg-dev gcc libbz2-dev libc6-dev libjpeg62-turbo-dev libopenjp2-7-dev libpcre3-dev libssl-dev libtiff5-dev libxml2-dev libxslt1-dev wget zlib1g-dev python-pip python2.7-dev python-pathlib"
 
 passo(){
     local passo="$1"
@@ -14,26 +17,22 @@ passo(){
     local descricao="$3"
     echo "\n\n##### $descricao"
     if [ $vai -eq 0 ]; then    
-        if [ $passo = 'a' ]; then	
-            echo "A versao do plone será $PLONE_VERSION"
-        elif [ $passo = 'b' ]; then
+        if [ $passo = 'b' ]; then
             echo "" >> /etc/apt/sources.list
             echo "# Debian 9 para Python 2.7" >> /etc/apt/sources.list
             echo "deb http://archive.debian.org/debian/ stretch contrib main contrib non-free"  >> /etc/apt/sources.list
             apt update
-        elif [ $passo = 'c' ]; then
-            buildDeps="dpkg-dev gcc libbz2-dev libc6-dev libjpeg62-turbo-dev libopenjp2-7-dev libpcre3-dev libssl-dev libtiff5-dev libxml2-dev libxslt1-dev wget zlib1g-dev vim openssh-server python-pip python2.7-dev"
             apt update && apt install -y --no-install-recommends $buildDeps
         elif [ $passo = 'd' ]; then
             /sbin/useradd --system -m -d /plone -U -u 500 plone
             mkdir -p /plone/instance/ /data/filestorage /data/blobstorage
-            cp buildout.cfg /plone/instance
-        elif [ $passo = 'e' ]; then
-            echo "Feito no passo d."
-        elif [ $passo = 'f' ]; then
-            echo "Feito nos passos referentes às instalações."
-        elif [ $passo = 'g' ]; then
-            echo "Instala junto com a primeira parte das dependências."
+            wget -O /plone/instance/buildout.cfg https://raw.githubusercontent.com/plonegovbr/portalpadrao.docker/refs/tags/v2.1.1/buildout.cfg
+            wget -O /plone/instance/portal-padrao-versions.cfg https://raw.githubusercontent.com/plonegovbr/portalpadrao.release/master/$PORTAL_PADRAO/versions.cfg
+            echo "# Outras versões pinadas" >> /plone/instance/portal-padrao-versions.cfg
+            echo "cssselect = 0.7" >> /plone/instance/portal-padrao-versions.cfg
+            echo "ijson = 2.3" >> /plone/instance/portal-padrao-versions.cfg
+            echo "inflection = 0.3.1" >> /plone/instance/portal-padrao-versions.cfg
+            echo "backports.functools-lru-cache = 1.6.4" >> /plone/instance/portal-padrao-versions.cfg
         elif [ $passo = 'h' ]; then
             wget -O Plone.tgz https://launchpad.net/plone/$PLONE_MAJOR/$PLONE_VERSION/+download/Plone-$PLONE_VERSION-UnifiedInstaller.tgz
             echo "$PLONE_MD5 Plone.tgz" | md5sum -c -
@@ -42,6 +41,7 @@ passo(){
             cp -v ./Plone-$PLONE_VERSION-UnifiedInstaller/buildout_templates/buildout.cfg /plone/instance/buildout-base.cfg
         elif [ $passo = 'i' ]; then
             pip install pip==$PIP setuptools==$SETUPTOOLS zc.buildout==$ZC_BUILDOUT wheel==$WHEEL
+            pip install psutil==6.1.* argcomplete
         elif [ $passo = 'j' ]; then
             cd /plone/instance
             buildout
@@ -69,14 +69,9 @@ limpar()
     rm -rf /plone/buildout-cache/downloads/*
 }
 
-passo "a" 1 "Exportando variaveis"
-passo "b" 1 "Repositorio para python 2.7"
-passo "c" 0 "Instalando pacotes iniciais"
-passo "d" 0 "Adicionando usuario plone" 
-passo "e" 0 "Arquivo buildout.cfg"
-passo "f" 0 "Setando variaveis de pacote"
-passo "g" 0 "Instalando dependencias 1"
-passo "h" 1 "Instalando Plone"
-passo "i" 1 "Pinando versoes"
-passo "j" 1 "Rodando o buildout"
-passo "k" 1 "Pastas 'data' e 'plone'"
+passo "b" 0 "Repositorio python 2.7; Instalação"
+passo "d" 0 "Adicionando plone; Arquivos cfg"
+passo "h" 0 "Instalando Plone"
+passo "i" 0 "Pinando versoes"
+passo "j" 0 "Rodando o buildout"
+passo "k" 0 "Pastas 'data' e 'plone'"
